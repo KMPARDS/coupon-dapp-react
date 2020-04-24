e930a6e72a631cb5c646df323c5892d84ca28666
zemse <sohamzemse143@gmail.com>
2020-04-24 17:20:09 +0530

    basic format navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
+import { Navbar, Dropdown } from 'react-bootstrap';
 import NavbarElement from './NavbarElement';
 
 export default class extends Component {
   render = () => (
     <header className="header_area menu_two header_style">
       <div className="header_menu">
-		    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Navbar bg="dark" expand="lg">
 			    <a className="navbar-brand" href="index.html">
 					  <img className="main_logo" src="img/Coupondapp-logo.png" alt="" width="180" />
 					  <img className="mobile_logo" src="img/Coupondapp-logo-blue.png" width="230"  alt="" />
           </a>
-				  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					 <span></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
 					  <ul className="nav navbar-nav ml-auto">
   						<NavbarElement name="Home" path="/" parentProps={this.props} />
   						<NavbarElement name="New Coupon" path="/new" parentProps={this.props} />
   						<NavbarElement name="Redeem Coupon" path="/redeem" parentProps={this.props} />
-  						<li className="">
-  							<a className="">Load Wallet</a>
+  						<li className="navbar-element">
+                <Dropdown alignRight>
+                  <Dropdown.Toggle variant="outline-light" id="dropdown-basic">
+                    Dropdown Button
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="login-dropdown-color">
+                    <Dropdown.Item href="#/action-1">Using Era Swap Life</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Metamask</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+
   						</li>
   					</ul>
-  					<ul className="nav navbar-nav navbar-right">
-  						<li>
-                <a className="popup-with-zoom-anim" href="#test-search"><i className="icon icon-Search"></i></a>
-              </li>
-  					</ul>
-  				</div>
-  			</nav>
+  				</Navbar.Collapse>
+  			</Navbar>
       </div>
     </header>
   );
