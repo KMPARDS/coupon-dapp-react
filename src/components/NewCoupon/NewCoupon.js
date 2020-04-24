f643a304b8aff4548cd26ddc2f0b7c662f7b9b69
zemse <sohamzemse143@gmail.com>
2020-04-25 03:14:49 +0530

    add coupon file download

--- src/components/NewCoupon/NewCoupon.js
+++ src/components/NewCoupon/NewCoupon.js
@@ -6,9 +6,13 @@ const assert = require('assert');
 
 export default class extends Component {
   state = {
+    currentScreen: 0,
     userBalance: null,
     esAmount: '',
     inputError: '',
+    couponBytes: '',
+    spinner0: false,
+    spinner1: false,
   };
 
   intervalId = null;
@@ -52,75 +56,157 @@ export default class extends Component {
           </div>
           <div className="col-12 col-lg-5 offset-1 align-self-center">
             <div className="v2 welcome-progress create-coupon-form-1">
-              <InputGroup
-                className={this.state.inputError ? 'red-border' : null}
-              >
-                <FormControl
-                  placeholder="Enter coupon amount"
-                  className={['large'].filter((c) => !!c).join(' ')}
-                  value={this.state.esAmount}
-                  onChange={(event) => {
-                    const newState = {};
-                    newState.inputError = '';
-                    try {
-                      try {
-                        ethers.utils.parseEther(event.target.value || '0');
-                      } catch (error) {
-                        throw new Error(
-                          'Invalid amount: ' + event.target.value
-                        );
-                      }
+              {(() => {
+                switch (this.state.currentScreen) {
+                  case 0:
+                    return (
+                      <>
+                        <InputGroup
+                          className={
+                            this.state.inputError ? 'red-border' : null
+                          }
+                        >
+                          <FormControl
+                            placeholder="Enter coupon amount"
+                            className={['large'].filter((c) => !!c).join(' ')}
+                            value={this.state.esAmount}
+                            onChange={(event) => {
+                              const newState = {};
+                              newState.inputError = '';
+                              try {
+                                try {
+                                  ethers.utils.parseEther(
+                                    event.target.value || '0'
+                                  );
+                                } catch (error) {
+                                  throw new Error(
+                                    'Invalid amount: ' + event.target.value
+                                  );
+                                }
 
-                      newState.esAmount = event.target.value;
+                                newState.esAmount = event.target.value;
 
-                      assert.ok(
-                        !!this.state.userBalance,
-                        'Wallet not loaded. Please load you wallet by clicking connect wallet in Navbar'
-                      );
+                                assert.ok(
+                                  !!this.state.userBalance,
+                                  'Wallet not loaded. Please load you wallet by clicking connect wallet in Navbar'
+                                );
 
-                      assert.ok(
-                        this.state.userBalance.gte(
-                          ethers.utils.parseEther(event.target.value || '0')
-                        ),
-                        `Insufficient Balance${
-                          this.state.userBalance
-                            ? ` (${window.lessDecimals(
-                                this.state.userBalance
-                              )} ES)`
-                            : ''
-                        }`
-                      );
-                    } catch (error) {
-                      newState.inputError = error.message;
-                    }
+                                assert.ok(
+                                  this.state.userBalance.gte(
+                                    ethers.utils.parseEther(
+                                      event.target.value || '0'
+                                    )
+                                  ),
+                                  `Insufficient Balance${
+                                    this.state.userBalance
+                                      ? ` (${window.lessDecimals(
+                                          this.state.userBalance
+                                        )} ES)`
+                                      : ''
+                                  }`
+                                );
+                              } catch (error) {
+                                newState.inputError = error.message;
+                              }
 
-                    this.setState(newState);
-                  }}
-                />
-                <InputGroup.Append>
-                  <Button
-                    className="large"
-                    variant="outline-secondary"
-                    disabled
-                  >
-                    ES
-                  </Button>
-                </InputGroup.Append>
-              </InputGroup>
+                              this.setState(newState);
+                            }}
+                          />
+                          <InputGroup.Append>
+                            <Button
+                              className="large"
+                              variant="outline-secondary"
+                              disabled
+                            >
+                              ES
+                            </Button>
+                          </InputGroup.Append>
+                        </InputGroup>
 
-              {this.state.inputError ? (
-                <Alert variant="danger" style={{ marginTop: '1rem' }}>
-                  {this.state.inputError}
-                </Alert>
-              ) : null}
+                        {this.state.inputError ? (
+                          <Alert variant="danger" style={{ marginTop: '1rem' }}>
+                            {this.state.inputError}
+                          </Alert>
+                        ) : null}
 
-              <Button
-                disabled={!!this.state.inputError || !this.state.esAmount}
-                className="create-coupon-button"
-                block={true}
-              >
-                Create Coupon
-              </Button>
+                        <Button
+                          disabled={
+                            !!this.state.inputError ||
+                            !this.state.esAmount ||
+                            this.state.spinner0
+                          }
+                          className="create-coupon-button"
+                          block={true}
+                          onClick={() => {
+                            this.setState({ spinner0: true });
+                            const couponBytes = ethers.utils.randomBytes(64);
+                            setTimeout(() => {
+                              this.setState({
+                                currentScreen: 1,
+                                couponBytes,
+                                spinner0: false,
+                              });
+                            }, 1000);
+                          }}
+                        >
+                          {!this.state.spinner0 ? (
+                            <>Create Coupon</>
+                          ) : (
+                            <>Generating coupon...</>
+                          )}
+                        </Button>
+                      </>
+                    );
+                  case 1:
+                    return (
+                      <>
+                        <h3>Step 2</h3>
+                        Download the coupon by clicking below button. This
+                        coupon will be activated once all the steps are
+                        completed.
+                        <Button
+                          disabled={this.state.spinner1}
+                          block={true}
+                          className="create-coupon-button"
+                          onClick={() => {
+                            this.setState({ spinner1: true });
+                            setTimeout(() => {
+                              const element = document.createElement('a');
+                              const file = new Blob(
+                                [
+                                  window.generateCouponFileJson(
+                                    this.state.couponBytes
+                                  ),
+                                ],
+                                {
+                                  type: 'text/plain',
+                                }
+                              );
+                              element.href = URL.createObjectURL(file);
+                              element.download = `ES-${this.state.esAmount
+                                .split('.')
+                                .join('-')}-${ethers.utils
+                                .keccak256(this.state.couponBytes)
+                                .slice(2, 8)}.coupondapp`;
+                              document.body.appendChild(element); // Required for this to work in FireFox
+                              element.click();
+                              this.setState({
+                                spinner1: false,
+                                currentScreen: 2,
+                              });
+                            }, 1000);
+                          }}
+                        >
+                          {!this.state.spinner1 ? (
+                            <>Download</>
+                          ) : (
+                            <>Generating file...</>
+                          )}
+                        </Button>
+                      </>
+                    );
+                }
+              })()}
             </div>
           </div>
         </div>
